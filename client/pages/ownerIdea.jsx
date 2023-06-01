15ad3addc5c2f5ecaa3e18b87a44a41eedcbe05b
simrandhankar <simran.dhankar@itmtb.com>
2023-06-02 00:01:12 +0530

    ownerIdea and viewIdea merged

--- client/pages/ownerIdea.jsx
+++ client/pages/ownerIdea.jsx
@@ -9,7 +9,7 @@ import { MdCelebration } from "react-icons/md";
 import "semantic-ui-css/semantic.min.css";
 import { Input } from "semantic-ui-react";
 
-const ViewIdea = () => {
+const OwnerIdea = () => {
   const [owner, setOwner] = useState(false);
   const [phonenav, setPhonenav] = useState(false);
   const [modalClick, setModalClick] = useState(false);
@@ -192,61 +192,62 @@ const ViewIdea = () => {
           </div>
         </div>
 
-      {owner ? (
-        //Accept Proposals
-        <div className="bg-[#0C0634] flex flex-col text-white m-6 rounded-md sm:rounded-xl px-6 py-8 sm:mx-14 sm:my-10 ">
-          <div>
-            <p className="text-[#05EAFA]">List of Proposals</p>
-            <div className="grid grid-cols-1 gap-4 mx-6 cardsCollection sm:grid-cols-2 lg:grid-cols-3">
-              {/* card  */}
-              <ProposalCard />
-              <ProposalCard />
-              <ProposalCard />
-              <ProposalCard />
+        {owner ? (
+          //Accept Proposals
+          <div className="bg-[#0C0634] flex flex-col text-white m-6 rounded-md sm:rounded-xl px-6 py-8 sm:mx-14 sm:my-10 ">
+            <div>
+              <p className="text-[#05EAFA]">List of Proposals</p>
+              <div className="grid grid-cols-1 gap-4 mx-6 cardsCollection sm:grid-cols-2 lg:grid-cols-3">
+                {/* card  */}
+                <ProposalCard />
+                <ProposalCard />
+                <ProposalCard />
+                <ProposalCard />
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        //Send Proposal
-        <div className="bg-[#0C0634] flex flex-col text-white m-6 rounded-md sm:rounded-xl px-6 py-8 sm:mx-14 sm:my-10 ">
-          <div>
-            <p className="text-[#05EAFA]">Your Proposal</p>
-            <textarea
-              onChange={(e) => {
-                setDesc(e.target.value);
-              }}
-              value={desc}
-              name=""
-              id=""
-              cols="30"
-              rows="4"
-              className="w-full placeholder:font-semibold bg-inherit"
-              placeholder="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-            ></textarea>
+        ) : (
+          //Send Proposal
+          <div className="bg-[#0C0634] flex flex-col text-white m-6 rounded-md sm:rounded-xl px-6 py-8 sm:mx-14 sm:my-10 ">
+            <div>
+              <p className="text-[#05EAFA]">Your Proposal</p>
+              <textarea
+                onChange={(e) => {
+                  setDesc(e.target.value);
+                }}
+                value={desc}
+                name=""
+                id=""
+                cols="30"
+                rows="4"
+                className="w-full placeholder:font-semibold bg-inherit"
+                placeholder="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+              ></textarea>
+            </div>
+            <div className="flex sm:justify-end justify-center sm:mt-[2rem] mt-[5rem] sm:-mb-3">
+              {proposal ? (
+                <button
+                  className="bg-[#E40E82] opacity-20 flex items-center justify-between w-fit  rounded-2xl px-3 py-2 font-medium"
+                  disabled
+                >
+                  Send Proposal{" "}
+                  <BsFillArrowRightCircleFill size={20} className="ml-2" />
+                </button>
+              ) : (
+                <button
+                  onClick={printDesc}
+                  className="bg-[#E40E82] flex items-center justify-between w-fit  rounded-2xl px-3 py-2 font-medium"
+                >
+                  Send Proposal{" "}
+                  <BsFillArrowRightCircleFill size={20} className="ml-2" />
+                </button>
+              )}
+            </div>
           </div>
-          <div className="flex sm:justify-end justify-center sm:mt-[2rem] mt-[5rem] sm:-mb-3">
-            {proposal ? (
-              <button
-                className="bg-[#E40E82] opacity-20 flex items-center justify-between w-fit  rounded-2xl px-3 py-2 font-medium"
-                disabled
-              >
-                Send Proposal{" "}
-                <BsFillArrowRightCircleFill size={20} className="ml-2" />
-              </button>
-            ) : (
-              <button
-                onClick={printDesc}
-                className="bg-[#E40E82] flex items-center justify-between w-fit  rounded-2xl px-3 py-2 font-medium"
-              >
-                Send Proposal{" "}
-                <BsFillArrowRightCircleFill size={20} className="ml-2" />
-              </button>
-            )}
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
 
-export default ViewIdea;
+export default OwnerIdea;
